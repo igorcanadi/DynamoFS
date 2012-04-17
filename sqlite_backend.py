7a3058118c42e53465ddd399a48eb51f3f37d34a
sethpollen <none@none>
2012-04-16 20:11:41 -0500

    SQLite Backend is working from RAM (not persistent yet)

--- sqlite_backend.py
+++ sqlite_backend.py
@@ -14,39 +14,65 @@ class Datum(Base):
     value = Column(String) # TODO could specify string lengths by String(100) or similar
     refCount = Column(Integer)
     
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.refCount = 1
+    
 
 # Backend that uses a local SQLite database with one table. The table has
-# three columns: key, value, and refcount.
+# three columns: key, value, and refCount. We assume single-threaded use
+# of this backend.
 class SQLiteBackend:
     # filename is the local file for the SQLite database.
     def __init__(self, filename):
         # Open a connection to the database.
         engine = create_engine('sqlite:///:memory:', echo=True)
+        Base.metadata.create_all(engine)
+        
         Session = sessionmaker(bind=engine)
         self.session = Session()
 
     def __del__(self):
-        # TODO
-        pass
+        self.session.close()
+    
+    # Returns None if there is no datum with the given key.
+    def getDatum(self, key):
+        return self.session.query(Datum).filter_by(key=key).first()
 
     def put(self, value):
         key = hash.generateKey(value)
         
-        # TODO
+        # Query the datum, if it already exists.
+        datum = self.getDatum(key)
+        if datum is None:
+            # This key isn't in use yet. Set the refCount to 1.
+            self.session.add(Datum(key, value))
+        else:
+            # This key has already been written to, so just increment the
+            # reference count.
+            datum.refCount += 1
         
         self.session.commit()
-        pass
+        return key
 
     def get(self, key):
-        datum = self.session.query(Datum).filter_by(key=key).first()
-        return datum.value
+        datum = self.getDatum(key)
+        if datum is None:
+            return None
+        else:
+            return datum.value
 
     def incRefCount(self, key):
-        # TODO
+        self.getDatum(key).refCount += 1
         self.session.commit()
-        pass
         
     def decRefCount(self, key):
-        # TODO
+        datum = self.getDatum(key)
+        datum.refCount -= 1
+        
+        # Garbage collection.
+        if datum.refCount == 0:
+            self.session.delete(datum)
+        
         self.session.commit()
-        pass
