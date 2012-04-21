7a3c3e69dd2c78cbb81854de5b2be6b6ffb4b03a
seth.pollen@gmail.com <seth.pollen@gmail.com>
2012-04-21 10:44:51 -0500

    Fixed classpath error with Jython Java class loading

--- dynamodb_backend.py
+++ dynamodb_backend.py
@@ -1,7 +1,21 @@
 import sys
+import os
+   
+# Adds all the jar files in a directory to the path. Setting "recursive" to True
+# will cause this method to descending into directories, searching for JARs.
+def addJarsFrom(dirPath, recursive):
+    jars = os.listdir(dirPath)
+    for jar in jars:
+        jarPath = dirPath + "/" + jar
+        if os.path.isdir(jarPath):
+            if recursive:
+                addJarsFrom(jarPath, recursive)            
+        elif jar.endswith(".jar"):
+            sys.path.append(jarPath)
 
 # Initialize the classpath and import AWS Java classes.
-sys.path.append("aws-java-sdk-1.3.7/lib/aws-java-sdk-1.3.7.jar")
+addJarsFrom("aws-java-sdk-1.3.7/lib", False)
+addJarsFrom("aws-java-sdk-1.3.7/third-party", True)
 from java.io import *
 from java.util import *
 from com.amazonaws import *
@@ -9,8 +23,13 @@ from com.amazonaws.auth import *
 from com.amazonaws.services.dynamodb import *
 from com.amazonaws.services.dynamodb.model import *
 
-credentialsFile = "AwsCredentials.properties"
-tableName = "data"
+CREDENTIALS_FILE = "AwsCredentials.properties"
+TABLE_NAME = "data"
+
+# We use single-character attribute names to save space in requests.
+KEY = "k"
+VALUE = "v"
+REF_COUNT = "r"
 
 
 # Backend driven by DynamoDB. We use one table called "data", which has the
@@ -19,14 +38,23 @@ tableName = "data"
 #    refCount (number)
 #    value (string)
 class DynamoDBBackend:
+    
     def __init__(self):
         # Initialize the client library, using the credentials.
-        credentials = PropertiesCredentials(FileInputStream(credentialsFile))
+        credentials = PropertiesCredentials(FileInputStream(CREDENTIALS_FILE))
         self.client = AmazonDynamoDBClient(credentials)
+        self.capacityUsed = 0.0
+    
         
     # Makes an API Key object from a key string.
     def apiKey(self, key):
         return Key().withHashKeyElement(AttributeValue().withS(key))
+    
+    
+    # Increments the counter of capacity used.
+    def useCapacity(self, result):
+        self.capacityUsed += result.getConsumedCapacityUnits()
+
 
     def put(self, key, value):
         key = self.apiKey(key)
@@ -37,54 +65,98 @@ class DynamoDBBackend:
         updates = HashMap()
         
         # Increment the refCount if it exists, or set it to 1 if it doesn't exist.
-        updates.put("refCount", AttributeValueUpdate()
+        updates.put(REF_COUNT, AttributeValueUpdate()
                     .withAction(AttributeAction.ADD)
                     .withValue(AttributeValue().withN("1")))
         
         # Simply put the value.
-        updates.put("value", AttributeValueUpdate()
+        updates.put(VALUE, AttributeValueUpdate()
                     .withAction(AttributeAction.PUT)
                     .withValue(AttributeValue().withS(value)))
         
         request = (UpdateItemRequest()
-                   .withTableName(tableName)
+                   .withTableName(TABLE_NAME)
                    .withKey(key)
                    .withAttributeUpdates(updates))
-        result = self.client.updateItem(request) # TODO handle errors
-        # TODO collect capacity units consumed
+        result = self.client.updateItem(request)
+        self.useCapacity(result)
+
 
     def get(self, key):
         key = self.apiKey(key)
         
         # Issue an eventually-consistent GetItem request.
         request = (GetItemRequest()
+                   .withTableName(TABLE_NAME)
                    .withConsistentRead(False) # No strong consistency.
                    .withKey(key)
-                   .withAttributesToGet("value"))
-        result = self.client.getItem(request) # TODO handle errors
-        # TODO collect capacity units consumed
+                   .withAttributesToGet(Collections.singleton(VALUE)))
+        result = self.client.getItem(request)
+        self.useCapacity(result)
         
         item = result.getItem()
         if not(item is None):
-            return item.get("value").getS()
+            return item.get(VALUE).getS()
         
         else:
             # If the eventually consistent request failed, try a consistent GetItem request.
-            request = request.withConsistendRead(True)
+            request = request.withConsistentRead(True)
             result = self.client.getItem(request)
-            # TODO collect capacity units consumed.
+            self.useCapacity(result)
             
             if not(item is None):
-                return item.get("value").getS()
+                return item.get(VALUE).getS()
             
             else: # The key must not exist in the database.
                 raise KeyError
 
+
+    # Issues a request to add a specific delta value (integer) to the refCount for a key.
+    def addToRefCount(self, key, delta):
+        # Issue an UpdateItem request to increment the refCount.
+        updates = HashMap()
+        
+        # Increment the refCount.
+        updates.put(REF_COUNT, AttributeValueUpdate()
+                    .withAction(AttributeAction.ADD)
+                    .withValue(AttributeValue().withN(str(delta))))
+        
+        request = (UpdateItemRequest()
+                   .withTableName(TABLE_NAME)
+                   .withKey(key)
+                   .withAttributeUpdates(updates))
+        result = self.client.updateItem(request)
+        self.useCapacity(result)
+
+
     def incRefCount(self, key):
-        # TODO
-        pass
+        key = self.apiKey(key)
+        
+        # Issue an UpdateItem request to increment the refCount.
+        self.addToRefCount(key, 1)
+        
         
     def decRefCount(self, key):
-        # TODO
-        pass
+        key = self.apiKey(key)
+        
+        # Issue an UpdateItem request to decrement the refCount.
+        self.addToRefCount(key, -1)
+        
+        # Atomically delete the item, if its reference count is zero.
+        expectation = HashMap()
+        
+        # Check refCount.
+        expectation.put(REF_COUNT, ExpectedAttributeValue()
+                        .withValue(AttributeValue().withN("0")))
+        
+        request = (DeleteItemRequest()
+                   .withTableName(TABLE_NAME)
+                   .withKey(key)
+                   .withExpected(expectation))
+        
+        try:
+            result = self.client.deleteItem(request)
+            self.useCapacity(result)
+        except ConditionalCheckFailedException:
+            pass # Do nothing. This just means the refCount was positive.
         
