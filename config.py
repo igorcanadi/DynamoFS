ab7b43a3a4ab28d8791e42de7ce8386d10eee891
Igor Canadi <igor.canadi@gmail.com>
2012-04-25 18:36:50 -0500

    added config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+
+# number of (hash, value) pairs controller will keep in cache
+CONTROLLER_CACHE_SIZE = 50000
+
+CACHE_MANAGER_CACHE_SIZE = 10000
+
+# only when cache becames this big start cleaning it up
+ELASTIC_CACHE_OVERHEAD = 1.1
