73f8052d8db3a5379eb187058d47344b8af143e1
Seth Pollen <seth.pollen@gmail.com>
2012-05-02 18:41:18 -0500

    Finished distributed benchmark

--- benchmark/dist_benchmark.py
+++ benchmark/dist_benchmark.py
@@ -1,3 +1,13 @@
 '''
 Standard benchmarking script for DynamoDB and S3 backends.
 '''
+import benchmarks
+
+DEPTH = 0
+FILE_SIZE = 4 * 4096 # Four blocks = 16K
+
+def runDynamoDB():
+    benchmarks.runDynamoDB(DEPTH, FILE_SIZE)
+    
+def runS3():
+    benchmarks.runS3(DEPTH, FILE_SIZE)
