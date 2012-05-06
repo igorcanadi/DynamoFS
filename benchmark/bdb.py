e2656696355418a1f59f1fe96504e6463ff20834
James Paton <paton@cs.wisc.edu>
2012-05-06 16:45:38 -0500

    added support for multiple trials to bdb.py and csv output

--- benchmark/bdb.py
+++ benchmark/bdb.py
@@ -13,24 +13,28 @@ def printUsage():
     print "Usage: %s <max file size>" % sys.argv[0]
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         printUsage()
         sys.exit(1)
     maxSize = int(sys.argv[1])
+    numTrials = 10
+    if len(sys.argv) == 3:
+        numTrials = int(sys.argv[2])
     for size in range(maxSize, 0, -maxSize / 20):
-        db = bsddb.hashopen("bdb.db")
-        totalWritten = 0
-        beginTime = time.time()
-        for block in range((size + PAGE_SIZE - 1) / PAGE_SIZE):
-            toWrite = min(PAGE_SIZE, size - totalWritten)
-            string = semirandomString(toWrite)
-            hash = hashlib.sha512(string).hexdigest()
-            db[hash] = string
-            totalWritten += toWrite
-        db.close()
-        endTime = time.time()
-        delta = endTime - beginTime
-        print "write,barebdb,%s" % str(delta)
+        for trial in range(numTrials):
+            db = bsddb.hashopen("bdb.db")
+            totalWritten = 0
+            beginTime = time.time()
+            for block in range((size + PAGE_SIZE - 1) / PAGE_SIZE):
+                toWrite = min(PAGE_SIZE, size - totalWritten)
+                string = semirandomString(toWrite)
+                hash = hashlib.sha512(string).hexdigest()
+                db[hash] = string
+                totalWritten += toWrite
+            db.close()
+            endTime = time.time()
+            delta = endTime - beginTime
+            print "write,barebdb,%d,%s" % (size, str(delta))
 
 if __name__ == '__main__':
     main()
