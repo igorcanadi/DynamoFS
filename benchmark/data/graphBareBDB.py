b615de2f954eb1eccf08c85ecce4b98f10908744
James Paton <paton@cs.wisc.edu>
2012-05-07 13:33:35 -0500

    added attempt to use Times New Roman; deleted some unused variables

--- benchmark/data/graphBareBDB.py
+++ benchmark/data/graphBareBDB.py
@@ -3,13 +3,14 @@ import sys
 from graphLocal import getAverages
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import rc
 
 __author__ = 'jpaton'
 
 def printUsage():
     print "Usage: %s <csv file name for raw bdb> <csv file name for dynamoFS> <filename for throughput> <filename for speedup>"
 
-def plotThroughput(data, filename, legendStrings):
+def plotThroughput(data, filename):
     ind = np.arange(20) + 0.1
     fig = plt.figure(figsize = (8, 4), dpi = 600)
     ax = fig.add_subplot(111)
@@ -37,31 +38,29 @@ def plotThroughput(data, filename, legendStrings):
     ax.set_xticklabels(xlabels[1::2])
     ax.set_ylabel("Throughput (MB/s)")
     ax.set_xlabel("File size (MB)")
-    ax.legend([rect], legendStrings, loc="best", prop={'size':6})
     plt.savefig(filename)
 
-def plotSpeedup(data_raw, data_dfs, filename, legendStrings):
+def plotSpeedup(data_raw, data_dfs, filename):
     ind = np.arange(20) + 0.1
     fig = plt.figure(figsize = (8, 4), dpi = 600)
     ax = fig.add_subplot(111)
     barwidth = 0.8
     xlabels = map(lambda x: float(x[0]) / 10**6, data_raw)
 
-    data_raw = [i[0] / i[1] for i in data_raw]
-    data_dfs = [i[0] / i[1] for i in data_dfs]
+    data_raw = [i[1] for i in data_raw]
+    data_dfs = [i[1] for i in data_dfs]
 
     rect = ax.bar(
         ind,
-        [data_dfs[i] / data_raw[i] for i in range(data_raw)],
+        [data_raw[i] / data_dfs[i] for i in range(len(data_raw))],
         width = barwidth,
         color = 'b',
     )
 
     ax.set_xticks((ind + barwidth / 2)[1::2])
     ax.set_xticklabels(xlabels[1::2])
-    ax.set_ylabel("Throughput (MB/s)")
+    ax.set_ylabel("Speedup ($\\mathrm{mean}(T_\\mathrm{raw}) / \\mathrm{mean}(T_\\mathrm{dfs})$)")
     ax.set_xlabel("File size (MB)")
-    ax.legend([rect], legendStrings, loc="best", prop={'size':6})
     plt.savefig(filename)
 
 def main():
@@ -80,10 +79,11 @@ def main():
     data_raw = [row for row in reader]
     data_raw = [(float(size), float(time)) for (size, time) in getAverages(data_raw).items()]
 
-    dfs_filename = sys.argv[3]
-    raw_fieldnames = [
+    dfs_filename = sys.argv[2]
+    dfs_fieldnames = [
         "benchtype",
         "fstype",
+        "depth",
         "size",
         "time",
     ]
@@ -91,8 +91,14 @@ def main():
     data_dfs = [row for row in reader]
     data_dfs = [(float(size), float(time)) for (size, time) in getAverages(data_dfs).items()]
 
-    plotThroughput(sorted(data_raw), sys.argv[2], ["Write"])
-    plotSpeedup(data_raw, data_dfs, sys.argv[4], ["Write"])
+    font = {
+        "family" : "Times-Roman"
+    }
+
+    rc('font', **font)
+
+    plotThroughput(sorted(data_raw), sys.argv[3])
+    plotSpeedup(sorted(data_raw), sorted(data_dfs), sys.argv[4])
 
 if __name__ == '__main__':
     main()
